7b6b84c7baa703e56e04f2364fb9b8ac60018a8a
Tjalali <47788273+Tjalali@users.noreply.github.com>
2019-02-21 22:08:24 -0600

    update main
    Add my name and dash

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -10,16 +10,16 @@ public class Main {
 class NamePrinter {
     class NamePrinter {
     /**
-     * Prints the names of the group members separated by plus signs.
+     * Prints the names of the group members separated by a dash.
      *
      * Team note: if you change the separator, make sure to change it in the comments.
      */
     public void printNames() {
-        String separator = "+";
+        String separator = "-";
 
         String[] names = {
                 "Jacob E.",
-                "Name 2",
+                "Taha J.",
                 "Name 3",
                 "Name 4",
                 "Name 5"};
