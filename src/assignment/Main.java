ac6079bd453ba8142f657e19a6c7f6a28eea5bda
FlameHarbinger <47788548+FlameHarbinger@users.noreply.github.com>
2019-02-21 21:07:57 -0600

    Updated Main.java
    Changed from being separated by dashes to being separated by commas

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -9,17 +9,17 @@ public class Main {
 
 class NamePrinter {
     /**
-     * Prints the names of the group members separated by plus signs.
+     * Prints the names of the group members separated by commas signs.
      *
      * Team note: if you change the separator, make sure to change it in the comments.
      */
     public void printNames() {
-        String separator = "-";
+        String separator = ",";
 
         String[] names = {
                 "Jacob E.",
                 "Taha J.",
-                "Name 3",
+                "John P.",
                 "Name 4",
                 "Name 5"};
 
