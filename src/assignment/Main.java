1c4ea0dd8d21f9850061306b2d35f2c07fbfa188
eatjac <eatjac@gmail.com>
2019-02-21 21:47:17 -0600

    Trying to upload locally first - step 1

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -8,14 +8,17 @@ public class Main {
 }
 
 class NamePrinter {
+    class NamePrinter {
     /**
-     * Prints the names of the group members separated by spaces.
+     * Prints the names of the group members separated by plus signs.
+     *
+     * Team note: if you change the separator, make sure to change it in the comments.
      */
     public void printNames() {
         String separator = "+";
 
         String[] names = {
-                "Name 1",
+                "Jacob E.",
                 "Name 2",
                 "Name 3",
                 "Name 4",
