92ae8371660f1520692b1dc73cf6204ff60e4124
Tjalali <47788273+Tjalali@users.noreply.github.com>
2019-04-09 21:34:09 -0500

    Playspace class
    adding a new playspace class

---
+++ Playspace.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Playspace {
+    Card[] discardPile = new Card[];
+    String Type;
+
+    public void Playspace(String type){
+
+    }
+
+    public void clearPlayspace(){
+        // clear the discard pile 
+        discardPile = null;
+    }
+
+}
